70a78f9fc4fae1ea2138a102eb22b90fc1c31d98
Sylvain JEANPIERRE <112515717+sylvainjeanpierre@users.noreply.github.com>
2024-03-23 16:32:27 +0100

    Added collapse content to a-propos page

--- src/pages/APropos.js
+++ src/pages/APropos.js
@@ -8,13 +8,13 @@ function APropos() {
     <div id="a-propos">
       <Banner image={aProposBackground} />
 
-      <Collapse title="Fiabilité" content="Zone de texte" />
+      <Collapse title="Fiabilité" content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes." />
 
-      <Collapse title="Respect" content="Zone de texte" />
+      <Collapse title="Respect" content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." />
 
-      <Collapse title="Service" content="Zone de texte" />
+      <Collapse title="Service" content="Kasa s'engage à fournir un service de qualité à ses clients. Notre équipe est disponible 7j/7 pour répondre à vos questions et vous aider à trouver le logement idéal." />
 
-      <Collapse title="Sécurité" content="Zone de texte" />
+      <Collapse title="Sécurité" content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." />
 
     </div>
   );
